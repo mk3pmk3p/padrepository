b9f51b95cc6981c57f86485018c98184b55220e5
mk3pmk3p <mk3pmk3p@mk3pmk3p-PC>
2019-10-13 03:06:23 +0800

    update

--- PadPowerRanking/src/main/java/com/padrank/link/controller/PadController.java
+++ PadPowerRanking/src/main/java/com/padrank/link/controller/PadController.java
@@ -1,13 +1,30 @@
 package com.padrank.link.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.padrank.link.paramform.ParamForm;
 
 @Controller
 public class PadController {
 
 	@RequestMapping("/index")
-	public String main() {
+	public String main(ModelMap map) {
+		map.put("cityname", "Tokyo");
 		return "index";
 	}
+	@RequestMapping("/getData")
+	public ResponseEntity<?> main(ParamForm form) {
+		
+		List<String> list = new ArrayList<String>();
+		list.add(form.getName());
+		list.add(form.getNumber());
+		return ResponseEntity.ok(list);
+	}
+	
 }

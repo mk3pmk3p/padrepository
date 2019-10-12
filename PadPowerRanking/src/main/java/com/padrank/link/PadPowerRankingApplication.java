4fb7c85cde0397460721f49cf47355ec9c216da7
mk3pmk3p <mk3pmk3p@mk3pmk3p-PC>
2019-10-13 02:48:51 +0800

    update

---
+++ PadPowerRanking/src/main/java/com/padrank/link/PadPowerRankingApplication.java
@@ -0,0 +1,13 @@
+package com.padrank.link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadPowerRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadPowerRankingApplication.class, args);
+	}
+
+}
